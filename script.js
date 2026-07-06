33811ec37abba23d1a8efbfa25e074594597ddb3
shyam <shyambadagala@gmail.con>
2026-07-06 22:08:09 +0530

    added dynamic functionlaity

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const BASE_URL = "https://api.frankfurter.dev/v1/latest";
 //https://api.frankfurter.app/latest?from=USD&to=INR
 const selectopt = document.querySelectorAll(".dropdown select");
-const fromcurrency = document.querySelector(".from select");
-const tocurrency = document.querySelector(".to select");
+let fromcurrency = document.querySelector(".from select");
+let tocurrency = document.querySelector(".to select");
 const amnt = document.querySelector(".container input");
 const bt = document.querySelector(".btn");
-
+let msg = document.querySelector(".msg");
 
 
 for (let select of selectopt) {
@@ -29,17 +29,26 @@ for (let select of selectopt) {
     }
   });
 }
-
-bt.addEventListener("click", async (e) => {
-  let t = tocurrency.value;
-  let f = fromcurrency.value;
-  let v = amnt.value;
+fromcurrency.addEventListener("change", async (e) => {
+  fromcurrency = e.target.value;
   e.preventDefault();
-  const URL = `${BASE_URL}?from=${f}&to=${t}`;
+  const URL = `${BASE_URL}?from=${fromcurrency}&to=${tocurrency.value}`;
   let response = await fetch(URL);
   let data = await response.json();
-  let rate = data.rates[t];
+  let rate = data.rates[tocurrency.value];
   console.log(rate);
-    let msg = document.querySelector(".msg");
-    msg.innerText = `${v} ${f} = ${v*rate} ${t}`;
+  let msg = document.querySelector(".msg");
+  msg.innerText = `1 ${fromcurrency} = ${rate} ${tocurrency.value}`;
+});
+
+bt.addEventListener("click", async (e) => {
+  let v = amnt.value;
+  e.preventDefault();
+  const URL = `${BASE_URL}?from=${fromcurrency.value}&to=${tocurrency.value}`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data.rates[tocurrency.value];
+  console.log(rate);
+  let msg = document.querySelector(".msg");
+  msg.innerText = `${v} ${fromcurrency.value} = ${v * rate} ${tocurrency.value}`;
 });
